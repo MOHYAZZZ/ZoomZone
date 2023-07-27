1f6cafc8fd9a607bc5bbfe2b932c9d3986c2c71f
MOHYAZZZ <mohammadreza.yazdian.2020@live.rhul.ac.uk>
2023-07-27 17:29:46 +0800

    set up project

---
+++ zoom-zone/src/App.js
@@ -0,0 +1,44 @@
+
+const App = () =>  {
+  return (
+    <div className="Categories-container">
+      <div>
+        {/* <img> </img> */}
+        <div className="category-body-container">
+          <h2>35mm cameras</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div>
+        {/* <img> </img> */}
+        <div className="category-body-container">
+          <h2>120 cameras</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div>
+        {/* <img> </img> */}
+        <div className="category-body-container">
+          <h2>Disposable Cameras</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div>
+        {/* <img> </img> */}
+        <div className="category-body-container">
+          <h2>Digital Cameras</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div>
+        {/* <img> </img> */}
+        <div className="category-body-container">
+          <h2>Large format cameras</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
