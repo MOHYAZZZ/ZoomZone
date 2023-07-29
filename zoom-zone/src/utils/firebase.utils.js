09bbe85758eef1179344a098a84a373ed7f3df9e
MOHYAZZZ <mohammadreza.yazdian.2020@live.rhul.ac.uk>
2023-07-30 00:30:04 +0800

    add sign in component, firebase utils and add firebase libraries

---
+++ zoom-zone/src/utils/firebase.utils.js
@@ -0,0 +1,54 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBHc5rvs4kCA8kUzGUU6NhoRwlN7uiXJso",
+    authDomain: "zoom-zone-db.firebaseapp.com",
+    projectId: "zoom-zone-db",
+    storageBucket: "zoom-zone-db.appspot.com",
+    messagingSenderId: "656469154000",
+    appId: "1:656469154000:web:c308b6eb0a2db5735066d5"
+  };
+  
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: "select_account"
+}); 
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocFromAuth = async (userAuth) =>{
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    if(!userSnapshot.exists()){
+        const { displayName, email } = userAuth;
+        const createAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName, 
+                email,
+                createAt
+            });
+        } catch (error){
+            console.log('error creating the user', error.message);
+        }
+    }
+    return userDocRef;
+}
