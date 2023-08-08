9bb2366c679bd36798df860a7143e1b8b21d2ede
MOHYAZZZ <mohammadreza.yazdian.2020@live.rhul.ac.uk>
2023-08-08 18:24:38 +0800

    create checkout page along with checkout items and checkout cart functionality

--- zoom-zone/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ zoom-zone/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { CartContext } from "../../contexts/cart.context";
 
@@ -9,14 +10,22 @@ import "./cart-dropdown.styles.scss";
 
 const CartDropDown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckOutHandler = () => {
+    navigate("/checkout");
+  };
+
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-item">
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
+      <div className="cart-items">
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckOutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
